4b23fa544ffe9c4323837cd55a1b77358217eb77
ptkwalczak@gmail.com <ptkwalczak@gmail.com>
2021-04-28 13:00:05 +0200

    add debug logging requests

--- src/bug/bugs.controllers.ts
+++ src/bug/bugs.controllers.ts
@@ -3,19 +3,24 @@ import {
   Controller,
   Delete,
   Get,
+  Logger,
   Param,
   Patch,
   Post
 } from '@nestjs/common';
+import { AppController } from '../app.controller';
 import { BugsService } from './bugs.service';
 import { BugState } from './create-bug.dto';
 
 @Controller('bugs')
 export class BugsController {
+  private readonly logger = new Logger(AppController.name);
+
   constructor(private readonly bugsService: BugsService) {}
 
   @Get()
   getAllBugs(): any {
+    this.logger.debug('Getting all bugs');
     return this.bugsService.findAll();
   }
   @Post()
@@ -23,17 +28,20 @@ export class BugsController {
     @Body('title') bugTitle: string,
     @Body('description') bugDescription: string,
   ): any {
+    this.logger.debug(`Adding bug ${bugTitle}`);
     return this.bugsService.create(bugTitle, bugDescription);
   }
 
   @Get(':id')
   getBugById(@Param('id') bugId: string): any {
+    this.logger.debug(`Getting bug by id: ${bugId}`);
     return this.bugsService.findOne(bugId);
   }
 
   @Delete(':id')
-  deleteBugById(@Param('id') todoId: string): any {
-    return this.bugsService.deleteById(todoId);
+  deleteBugById(@Param('id') bugId: string): any {
+    this.logger.debug(`Deleting bug by id: ${bugId}`);
+    return this.bugsService.deleteById(bugId);
   }
   @Patch(':id')
   updateBugById(
@@ -42,6 +50,7 @@ export class BugsController {
     @Body('description') description: string,
     @Body('state') state: BugState,
   ): any {
+    this.logger.debug(`Updating bug by id: ${id}`);
     return this.bugsService.updateById(id, title, description, state);
   }
 }
